943c3b96d10928fd9879a2c5b4df74f17875755e
Swayam Prakash Sahu <91021799+SwayamSahu@users.noreply.github.com>
2024-05-31 23:30:00 +0530

    Update leap-year.py

--- leap-year.py
+++ leap-year.py
@@ -3,7 +3,7 @@
 year = 2000 
 
 # To get year (integer input) from the user
-# year = int(input("Enter a year: "))...........
+# year = int(input("Enter a year: "))
 
 # divided by 100 means century year (ending with 00)
 # century year divided by 400 is leap year
